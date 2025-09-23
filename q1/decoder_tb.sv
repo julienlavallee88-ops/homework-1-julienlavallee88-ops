5edc9c4eaf0065d039492132a092bb28fa15f620
julienlavallee88-ops <julien.lavallee88@gmail.com>
2025-09-23 19:11:36 +0200

    Update decoder_tb.sv

--- q1/decoder_tb.sv
+++ q1/decoder_tb.sv
@@ -1,14 +1,18 @@
-module decoder_tb;
+`timescale 1ns/1ns
+module decoder_TB;
+	logic [3:0] binary;
+	logic [15:0] one_hot;
 
-  // Testbench signals
-  logic [3:0] binary;
-  logic [15:0] one_hot;
+	decoder UUT ( .binary(binary), .one_hot(one_hot) );
 
-  // Instantiate the decoder module
-  decoder uut (
-    .binary(binary),
-    .one_hot(one_hot)
-  );
-
-  // Complete your testbench here
+	initial begin
+		
+		for (int i =0; i<16; i++) begin
+			binary = i[3:0];
+			#10;
+			$display("%0t bin=%04b one_hot=%016b", $time, binary, one_hot);
+		end
+	end
 endmodule
+
+		
