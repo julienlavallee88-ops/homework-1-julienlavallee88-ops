0688ccd2d99e0098bff0f224bf353eb6bb77f4fe
julienlavallee88-ops <julien.lavallee88@gmail.com>
2025-09-23 19:13:13 +0200

    Update csa_8.sv

--- q3/csa_8.sv
+++ q3/csa_8.sv
@@ -1,9 +1,29 @@
-module csa_8 (
-  input logic [7:0] a, b,
-  output logic [7:0] sum,
-  output logic carry
+// 4-bit adder
+module adder_4 (
+    input  logic [3:0] a, b,
+    input  logic       cin,
+    output logic [3:0] sum,
+    output logic       cout
 );
-  // …
-  // Add your description here
-  // …
+    assign {cout, sum} = a + b + cin;
 endmodule
+
+
+// 8-bit carry select adder
+module CSA_8 (
+    input  logic [7:0] a, b,
+    input  logic       cin,
+    output logic [7:0] sum,
+    output logic       cout
+);
+    // lower 4 bits
+    logic [3:0] sum_lo;
+    logic       cout_lo;
+
+    adder_4 adder_lo (
+        .a   (a[3:0]),
+        .b   (b[3:0]),
+        .cin (cin),
+        .sum (sum_lo),
+        .cout(cout_lo)
+    );
