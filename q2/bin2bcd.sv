feba2a3ace17c0e4a11798933ad55afda4466a7e
julienlavallee88-ops <julien.lavallee88@gmail.com>
2025-09-23 19:12:07 +0200

    Update bin2bcd.sv

--- q2/bin2bcd.sv
+++ q2/bin2bcd.sv
@@ -1,9 +1,19 @@
 module bin2bcd (
-  input logic [3:0] binary,
-  output logic [3:0] bcd,
-  output logic carry
+ input logic [3:0] binary,
+ output logic [3:0] bcd,
+ output logic carry
 );
-  // …
-  // Add your description here
-  // …
+always_comb begin
+    bcd = 4'b0;
+    carry = 1'b0;
+
+    if (binary < 4'd10) begin
+        carry = 1'b0;
+        bcd[3:0] = binary[3:0];
+    end else if (binary >= 10) begin
+        carry = 1'b1;
+        bcd[3:0] = binary - 4'd10;
+    end
+
+    end
 endmodule
