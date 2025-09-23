e4a64f19de6e01674bb0af5017d279acc1d1a932
julienlavallee88-ops <julien.lavallee88@gmail.com>
2025-09-23 19:12:26 +0200

    Update bin2bcd_tb.sv

--- q2/bin2bcd_tb.sv
+++ q2/bin2bcd_tb.sv
@@ -1,9 +1,20 @@
+`timescale 1ns/1ns
 module bin2bcd_tb;
+    logic [3:0] binary;
+    logic [3:0] bcd;
+    logic carry;
 
-  // Testbench signals
-  logic [3:0] binary;
-  logic [3:0] bcd;
-  logic carry;
-
-  // complete your tb
+    bin2bcd UUT (
+        .binary(binary),
+        .bcd(bcd),
+        .carry(carry)
+    );
+    initial begin
+        for (int i =0; i<17; i++) begin
+            binary =i[3:0];
+            #20;
+	    $display("%0t: binary=%d -> bcd=%d carry=%b", 
+                     $time, binary, bcd, carry);
+        end
+    end
 endmodule
