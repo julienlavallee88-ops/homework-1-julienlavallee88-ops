6602846e1cb28179a2826850c8b0b1bcef02e901
julienlavallee88-ops <julien.lavallee88@gmail.com>
2025-09-23 19:10:56 +0200

    Update decoder.sv

--- q1/decoder.sv
+++ q1/decoder.sv
@@ -1,8 +1,9 @@
-module decoder (
-  input logic [3:0] binary,
-  output logic [15:0] one_hot
+module decoderV2 (
+    input  logic [3:0]  binary,
+    output logic [15:0] one_hot
 );
-  // …
-  // Add your description here
-  // …
+    always_comb begin
+        one_hot = 16'b0;          // default = all zeros
+        one_hot[binary] = 1'b1;   // set one bit based on binary input
+    end
 endmodule
